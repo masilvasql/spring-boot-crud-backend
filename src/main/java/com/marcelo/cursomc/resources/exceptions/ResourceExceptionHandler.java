81dc35210a7a854c7ca1d2db4991f7f586131572
Marcelo Abrão da Silva <masilvasql@gmail.com>
2022-03-09 11:14:22 -0300

    Criado classe auxiliar de erro; Criado tratativa para melhorar a mensagem de error retornada.

--- src/main/java/com/marcelo/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/marcelo/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import com.marcelo.cursomc.services.exceptions.DataIntegrityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,6 +24,16 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e){
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	//MethodArgumentNotValidException
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> argumentValidation(MethodArgumentNotValidException e){
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		for(FieldError error : e.getBindingResult().getFieldErrors()){
+			err.addError(error.getField(), error.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
