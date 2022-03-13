327a3ae31ff9f7a2bba8926d14f3a3e3d9593c29
Marcelo Abrão da Silva <masilvasql@gmail.com>
2022-03-13 13:21:41 -0300

    Criando profile de teste

---
+++ src/main/java/com/marcelo/cursomc/config/TestConfig.java
@@ -0,0 +1,58 @@
+package com.marcelo.cursomc.config;
+
+import com.marcelo.cursomc.domain.*;
+import com.marcelo.cursomc.domain.enums.EstadoPagamento;
+import com.marcelo.cursomc.domain.enums.TipoCliente;
+import com.marcelo.cursomc.repositories.*;
+import com.marcelo.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        dbService.instantiateTestDatabase();
+
+        return true;
+    }
+}
